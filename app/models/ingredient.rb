8a68d7b4c9fa47f24200bc345032f3b36a4287a4
Morgan Autret <morgan.autret@gmail.com>
2017-08-22 12:43:55 +0200

    cwave added ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,5 @@ class Ingredient < ApplicationRecord
   has_many :doses
   validates :name, presence: true, uniqueness: true
   validates :january, presence: true, inclusion: { :in => [0, 50, 100]}
+  mount_uploader :photo, PhotoUploader
 end
