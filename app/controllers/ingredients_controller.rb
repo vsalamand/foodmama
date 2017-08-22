292139dad9b80a4e07201ba6fe8a0d6b49e369f9
Vincent Salamand <salamandvincent@gmail.com>
2017-08-22 12:40:07 +0200

    add create ingredient

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -7,4 +7,22 @@ class IngredientsController < ApplicationController
   def show
     @ingredient = Ingredient.find(params[:id])
   end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to ingredient_path(@ingredient)
+    else
+      render :new
+    end
+  end
+
+  private
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :january, :february, :march, :april, :may, :june, :july, :august, :september, :october, :november, :december)
+  end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,5 @@
 class Ingredient < ApplicationRecord
   has_many :doses
+  validates :name, presence: true, uniqueness: true
+  validates :january, presence: true, inclusion: { :in => [0, 50, 100]}
 end
