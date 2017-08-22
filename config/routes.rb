a31f748d022acd6cde3fd0b35451704b71d756bf
Morgan Autret <morgan.autret@gmail.com>
2017-08-22 11:18:49 +0200

    all routes + CRUD recipes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :recipes do
+    resources :doses, only: [:new, :create, :edit, :update, :destroy]
+  end
+
+  resources :ingredients
+
 end
+
+
+
