259a2cc3aae58d7796e46fdff34b827b61dcce41
alexisbonhoure <alexis.bonhoure13@gmail.com>
2017-08-22 15:23:00 +0200

    doses controller 2

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,6 +1,3 @@
 class Ingredient < ApplicationRecord
-  has_many :doses, dependent: :destroy
-  validates :name, presence: true, uniqueness: true
-  validates :january, presence: true, inclusion: { :in => [0, 50, 100]}
-  mount_uploader :photo, PhotoUploader
+  has_many :doses,
 end
