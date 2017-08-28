a28ce24c0f12b06e48826bf2f2d9578b09f1cb49
Morgan Autret <morgan.autret@gmail.com>
2017-08-28 15:52:46 +0200

    show optimisée, image ingrédients, affichage par étape et création méthode tri de strings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,20 @@
 module ApplicationHelper
+
+
+  def instruction_lines(recipe)
+    i = 1
+    html = []
+    recipe.instructions.split("\r\n").each do |line|
+      unless line == "" || line == " "
+        html << "<h3>Etape #{i}</h3><p>#{line}</p>"
+        i+=1
+      end
+    end
+    return html.join.html_safe
+  end
+
 end
+
+
+
+
