4b3e236f7ee16a5d5c061890268bef46a1326a16
Olivier Chuiton <olivier.chuiton@gmail.com>
2017-08-31 11:13:02 +0200

    refacto

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,22 +3,4 @@ class PagesController < ApplicationController
 
   def home
   end
-
-  def suggest
-    @recipes = Recipe.all
-    @user = current_user
-    @banned_ingredients = @user.find_down_voted_items
-    @month = Date.today.strftime("%B").downcase
-    @month_recipes = @recipes.reject do |recipe|
-        (recipe.ingredients & @banned_ingredients).any? || recipe.ingredients.any? { |ingredient| ingredient.send(@month) == 0 }
-      end
-    @suggested_recipes = @month_recipes.shuffle.take(2)
-  end
-
-  def search
-    @recipes = Recipe.all
-    if params[:query].present?
-      @search = Recipe.search(params[:query]).order("created_at DESC")
-    end
-  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,40 +1,10 @@
 class RecipesController < ApplicationController
-  before_action :set_recipe, only: [ :show, :edit, :destroy, :upvote, :downvote ]
+  before_action :set_recipe, only: [ :show, :upvote, :downvote ]
   skip_before_action :authenticate_user!, only: [ :show ]
-
-  # def index
-  #   @recipes = Recipe.all
-  # end
 
   def show
     @dose = Dose.new
   end
-
-  # def new
-  #   @recipe= Recipe.new
-  # end
-
-  # def create
-  #   @recipe= Recipe.new(recipe_params)
-  #   if @recipe.save
-  #     redirect_to recipe_path(@recipe)
-  #   else
-  #     render :new
-  #   end
-  # end
-
-  # def edit
-  # end
-
-  # def update
-  #   @recipe= Recipe.update(recipe_params)
-  #   redirect_to recipe_path(@recipe)
-  # end
-
-  # def destroy
-  #   @recipe.destroy
-  #    redirect_to recipes_path
-  # end
 
   def upvote
     @recipe.upvote_by current_user
@@ -47,7 +17,6 @@ class RecipesController < ApplicationController
   end
 
   def instruction_lines
-    raise
     i = 1
     @recipe.instructions.split("\r\n\r\n").each do |line|
       @line = line
@@ -62,6 +31,4 @@ class RecipesController < ApplicationController
   def recipe_params
     params.require(:recipe).permit(:name, :photo, :photo_cache, :instructions, :difficulty, :cooking_time, :preparation_time, :recipe_type_list, :diet_list, :season_list)
   end
-
-
 end
